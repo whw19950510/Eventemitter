bf2aa7d840ad191fb3cbd43443c6f8b9d18371d2
wanghuawei <whw19950510@gmail.com>
2017-11-11 15:42:25 -0600

    add file

---
+++ Eventemmiter.js
@@ -0,0 +1,29 @@
+
+
+class EventEmitter {
+    constructor() {
+        this.events={};
+    }
+    on(evetnName, callback) {
+        if(this.events[evetnName]) {
+            this.events[evetnName].push(callback);
+        } else {
+            this.events[evetnName]=[callback];//create new events
+        }
+    }
+    trigger(eventname, ...rest) {
+        if(this.events[evetnName])
+        {
+            this.events[eventName].forEach(cb=>{
+            cb.apply(null,rest)
+            })
+        }
+} 
+}  
+
+const ee=new EventEmitter();
+ee.on("change",()=>{
+    console.log("learning");
+})
+
+ee.trigger("change");
